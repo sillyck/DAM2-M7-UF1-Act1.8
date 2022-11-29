9f93c4e986acc3e460559a2a6632e8cbd32b046e
Jordi Ribellas <ribellas2002@gmail.com>
2022-11-29 17:56:45 +0100

    reorganitzat

--- src/CalculadoraBinaria.java
+++ src/CalculadoraBinaria.java
@@ -10,60 +10,65 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class CalculadoraBinaria extends JFrame {
-
+	private JTextField[] ipDecimal = new JTextField[5];
+	private JTextField ipMascara = new JTextField(3);
+	private JTextField[] ipBinari = new JTextField[5];
+	private JTextField[] ipMasBinari = new JTextField[5];
+	private JTextField[] xarxaBinari = new JTextField[5];
+	
 	public CalculadoraBinaria() {
 		
 		super("IP Clalculadora");
 		 setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		 setSize(800, 400);
 		 
-		 //Introduir IP
+		 //Decimal
 		 JPanel panelIP = new JPanel();
 		 panelIP.setLayout(new FlowLayout());
-		 panelIP.add(new JTextField(4));
+		 panelIP.add(ipDecimal[1]);
 		 panelIP.add(new JLabel("."));
-		 panelIP.add(new JTextField(4));
+		 panelIP.add(ipDecimal[2]);
 		 panelIP.add(new JLabel("."));
-		 panelIP.add(new JTextField(4));
+		 panelIP.add(ipDecimal[3]);
 		 panelIP.add(new JLabel("."));
-		 panelIP.add(new JTextField(4));
+		 panelIP.add(ipDecimal[4]);
 		 panelIP.add(new JLabel("/"));
-		 panelIP.add(new JTextField(4));
+		 panelIP.add(ipMascara);
 		 
 		 //Binari
 		 JPanel panelBinari = new JPanel();
 		 panelBinari.setLayout(new FlowLayout());
-		 panelBinari.add(new JTextField(5));
+		 panelBinari.add(ipBinari[1]);
 		 panelBinari.add(new JLabel("."));
-		 panelBinari.add(new JTextField(5));
+		 panelBinari.add(ipBinari[2]);
 		 panelBinari.add(new JLabel("."));
-		 panelBinari.add(new JTextField(5));
+		 panelBinari.add(ipBinari[3]);
 		 panelBinari.add(new JLabel("."));
-		 panelBinari.add(new JTextField(5));
+		 panelBinari.add(ipBinari[4]);
 		 
-		 //Mascara de Subred
+		 //Mascara Binari
 		 JPanel panelSubred = new JPanel();
 		 panelSubred.setLayout(new FlowLayout());
-		 panelSubred.add(new JTextField(5));
+		 panelSubred.add(ipMasBinari[1]);
 		 panelSubred.add(new JLabel("."));
-		 panelSubred.add(new JTextField(5));
+		 panelSubred.add(ipMasBinari[2]);
 		 panelSubred.add(new JLabel("."));
-		 panelSubred.add(new JTextField(5));
+		 panelSubred.add(ipMasBinari[3]);
 		 panelSubred.add(new JLabel("."));
-		 panelSubred.add(new JTextField(5));
+		 panelSubred.add(ipMasBinari[4]);
 		 
-		 //Mascara de Subred
+		 //XarxaBinari
 		 JPanel panelRed = new JPanel();
 		 panelRed.setLayout(new FlowLayout());
-		 panelRed.add(new JTextField(5));
+		 panelRed.add(xarxaBinari[1]);
 		 panelRed.add(new JLabel("."));
-		 panelRed.add(new JTextField(5));
+		 panelRed.add(xarxaBinari[2]);
 		 panelRed.add(new JLabel("."));
-		 panelRed.add(new JTextField(5));
+		 panelRed.add(xarxaBinari[3]);
 		 panelRed.add(new JLabel("."));
-		 panelRed.add(new JTextField(5));
+		 panelRed.add(xarxaBinari[4]);
 		 
-		//Grid organizat
+		 //Grid organizat
 		 JPanel panelDatos = new JPanel();
 		 GridLayout gl = new GridLayout(4,2);
 		 panelDatos.setLayout(gl);
@@ -83,14 +88,33 @@ public class CalculadoraBinaria extends JFrame {
 		 Container cp = getContentPane();
 		 cp.add(panelDatos, BorderLayout.CENTER);
 		 cp.add(boton, BorderLayout.SOUTH);
+		 
 	}
 	
-	private void omplirTextFiled (JTextField[] array, Container cp) {
-		
-		for(int i=0; i<array.length; i++) {
-			array[i] = new JTextField("");
-			cp.add(array[i]);
-			cp.add(new JLabel("."));
+	private void setTextIpDecimal() {
+		for(int i=1; i<5; i++) {
+			ipDecimal[i] = new JTextField(3);
+		}
+	}
+	
+	private void setTextBi() {
+		for(int i=1; i<5; i++) {
+			ipBinari[i] = new JTextField(8);
+			ipBinari[i].setEditable(false);
+		}
+	}
+	
+	private void setTextMascara() {
+		for(int i=1; i<5; i++) {
+			ipMasBinari[i] = new JTextField(8);
+			ipMasBinari[i].setEditable(false);
+		}
+	}
+	
+	private void setTextXarxaBinari() {
+		for(int i=1; i<5; i++) {
+			xarxaBinari[i] = new JTextField(8);
+			xarxaBinari[i].setEditable(false);
 		}
 	}
 	
